434fb20761825889d848fda0d208655604098eee
Justin Steensma <jeststeens@gmail.com>
2016-09-23 16:56:25 -0500

    5.1 and 5.2

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -11,66 +11,42 @@
 
 # print updated hash
 
-puts "what is the clients name?"
-client = gets.chomp
-puts "how old is the client?"
-age = gets.chomp
-puts "how many children does the client have?"
-children = gets.chomp
+client_info = {}
+
+puts "What is the client's name?"
+client_info[:name] = gets.chomp
+puts "How old is the client?"
+client_info[:age] = gets.chomp
+puts "How many children does the client have?"
+client_info[:number_of_children] = gets.chomp
 puts "What will be the decor theme?"
-decor = gets.chomp
+client_info[:decor] = gets.chomp
 
-client_info = {
-# :clients_name => client,
-# :clients_age => age.to_i,
-# :clients_number_children => children.to_i,
-# :decor_theme => decor
-}
 
-#make hash, no keys
-if client != "none"
-  client_info[:name] = client
-end
-
-if age != "none"
-  client_info[:clients_age] = age.to_i
-end
-
-if children != "none"
-client_info[:clients_number_children] = children.to_i
-end
-
-if decor != "none"
-  client_info[:decor_theme] = decor
-end
-
+# delete any key/values that are "none"
+require "pry"
+binding.pry
+client_info.keep_if{|key,value|value != "none" }
 
 puts client_info
 puts
 
-puts "Is all of this info correct?"
- correct_info = gets.chomp
- if correct_info == "no"
-  puts "What needs to be fixed? Name, Age, number of children, or decor?"
-  fix = gets.chomp
-else
-  puts "information logged!"
+correct_info = nil
+
+until correct_info == "yes" do
+  puts "Is all of this info correct?"
+  correct_info = gets.chomp
+
+  if correct_info == "no"
+    puts "What needs to be fixed? Name, Age, number of children, or decor?"
+    fix = gets.chomp
+    puts "enter correction"
+    client_info[fix.tr(" ","_").to_sym.downcase] = gets.chomp
+    puts client_info
+  else
+    puts "information logged!"
+  end
+
 end
 
-puts "enter correction"
-
-if fix == "name"
-  client = gets.chomp
-  client_info[:name] = client
-elsif fix ==  "age"
-  age = gets.chomp
-  client_info[:clients_age] = age.to_i
-elsif fix ==  "number of children"
-  children = gets.chomp
-  client_info[:clients_number_children] = children.to_i
-else
-  decor = gets.chomp
-  client_info[:decor_theme] = decor
-end
 puts
-puts client_info
